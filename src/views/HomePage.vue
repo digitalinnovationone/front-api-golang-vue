967a0222a6eb8f2c3db876d20b54137dd0911b02
Danilo Santos <danilo.aparecido.santos@gmail.com>
2024-03-05 21:19:54 -0300

    app vue jogo ingles

---
+++ src/views/HomePage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Jogo de palavras em ingles</h1>
+    <router-link to="/jogar">Começar a Jogar</router-link>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomePage',
+}
+</script>
